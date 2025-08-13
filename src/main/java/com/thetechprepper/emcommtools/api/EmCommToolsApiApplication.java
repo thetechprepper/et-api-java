6fc26cfb7835b4828f1a08bea1c997512f9532d0
thetechprepper <info@thetechprepper.com>
2025-08-13 05:37:44 -0700

    Fixed Vert.x cache directory creation failure when starting as a systemd unit file

--- src/main/java/com/thetechprepper/emcommtools/api/EmCommToolsApiApplication.java
+++ src/main/java/com/thetechprepper/emcommtools/api/EmCommToolsApiApplication.java
@@ -7,6 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EmCommToolsApiApplication {
 
     public static void main(String[] args) {
+	// ensure that we initialize the Vert.x cache for the gpsd library
+	// before Spring starts
+        System.setProperty("vertx.cacheDirBase", "/tmp");
+
         SpringApplication.run(EmCommToolsApiApplication.class, args);
     }
 }
