0cc071dcfbd688419d30936f6d212cf12441d4d8
thetechprepper <info@thetechprepper.com>
2025-08-17 03:10:58 -0700

    Added /api/geo/grid endpoint

--- src/main/java/com/thetechprepper/emcommtools/api/controller/PositionController.java
+++ src/main/java/com/thetechprepper/emcommtools/api/controller/PositionController.java
@@ -5,13 +5,17 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.thetechprepper.emcommtools.api.model.http.PositionResponseStatus;
 import com.thetechprepper.emcommtools.api.model.position.MaidenheadPosition;
 import com.thetechprepper.emcommtools.api.model.position.NullPosition;
 import com.thetechprepper.emcommtools.api.service.GpsService;
 import com.thetechprepper.emcommtools.api.service.UserService;
+import com.thetechprepper.emcommtools.api.util.GridSquareUtils;
 
 @RestController
 @RequestMapping("/api/geo")
@@ -34,7 +38,6 @@ public class PositionController {
                 MaidenheadPosition fallbackPosition = MaidenheadPosition.newInstance(
                     userService.getUserConfig().getGrid()
                 );
-                LOG.info("User: {}", userService.getUserConfig());
                 status.setPosition(fallbackPosition);
                 status.setHttpStatus(HttpStatus.OK.value());
             } catch(Exception e) {
@@ -42,6 +45,22 @@ public class PositionController {
                 status.setPosition(NullPosition.NO_POSITION);
                 status.setHttpStatus(HttpStatus.SERVICE_UNAVAILABLE.value());
             }
+        }
+
+        return new ResponseEntity<PositionResponseStatus>(status, HttpStatus.valueOf(status.getHttpStatus()));
+    }
+
+    @GetMapping(value = "/grid", produces = "application/json")
+    public ResponseEntity<PositionResponseStatus> getGrid(@RequestParam String gridSquare) {
+        PositionResponseStatus status = new PositionResponseStatus();
+
+        try {
+            status.setPosition(MaidenheadPosition.newInstance(gridSquare));
+            status.setHttpStatus(HttpStatus.OK.value());
+        } catch (Exception e) {
+            status.setPosition(NullPosition.NO_POSITION);
+            status.setHttpStatus(HttpStatus.BAD_REQUEST.value());
+            LOG.error("Error converting grid square to lat/lon", e);
         }
 
         return new ResponseEntity<PositionResponseStatus>(status, HttpStatus.valueOf(status.getHttpStatus()));
