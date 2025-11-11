8fd4e6e09f3216b079f996cb3613c7f9dc75d805
thetechprepper <info@thetechprepper.com>
2025-11-11 15:22:37 -0700

    Replaced VOACAP endpoint with POST. This is still a stub

--- src/main/java/com/thetechprepper/emcommtools/api/controller/VoacapController.java
+++ src/main/java/com/thetechprepper/emcommtools/api/controller/VoacapController.java
@@ -8,12 +8,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-//import com.thetechprepper.emcommtools.api.model.PredictionHour;
+import com.thetechprepper.emcommtools.api.model.VoacapRequest;
+// TODO import com.thetechprepper.emcommtools.api.model.PredictionHour;
 import com.thetechprepper.emcommtools.api.service.voacap.PredictionHour;
 import com.thetechprepper.emcommtools.api.service.voacap.VoacapOutputParser;
 
@@ -26,19 +28,24 @@ public class VoacapController {
     @Autowired
     private VoacapOutputParser parser;
 
-    @GetMapping(value = "/voacap", produces = "application/json")
-    public ResponseEntity<List<PredictionHour>> getPrediction() {
+    @PostMapping(value = "/voacap", consumes = "application/json", produces = "application/json")
+    public ResponseEntity<List<PredictionHour>> postPrediction(@RequestBody VoacapRequest request) {
 
+        LOG.info("Received VOACAP request: tx={}, rx={}, power={}, mode={}",
+                request.getTxLatLon(), request.getRxLatLon(), request.getPower(), request.getMode());
+
+	// TODO push into properties and use HOME env var
         final String path = "/home/gaston/itshfbc/run/voacapx.out";
 
         try {
             List<PredictionHour> hours = parser.parse(path);
+
             LOG.info("Parsed {} prediction hours", hours.size());
+
             return ResponseEntity.ok(hours);
         } catch (Exception e) {
             LOG.error("Failed to parse VOACAP output file: {}", path, e);
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(List.of());
-       }
-
+        }
     }
 }
