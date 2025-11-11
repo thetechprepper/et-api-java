48e98c075af4a78e82692cf335adfa9848c9c12e
thetechprepper <info@thetechprepper.com>
2025-11-11 15:42:05 -0700

    Added initial support for executing et-voacap. Validation and error handling still needed

--- src/main/java/com/thetechprepper/emcommtools/api/controller/VoacapController.java
+++ src/main/java/com/thetechprepper/emcommtools/api/controller/VoacapController.java
@@ -34,12 +34,52 @@ public class VoacapController {
         LOG.info("Received VOACAP request: tx={}, rx={}, power={}, mode={}",
                 request.getTxLatLon(), request.getRxLatLon(), request.getPower(), request.getMode());
 
+	// TODO add validation
+    
+	// Build command
+        List<String> command = List.of(
+                "/opt/emcomm-tools/bin/et-voacap",
+                "--tx-latlon", request.getTxLatLon(),
+                "--rx-latlon", request.getRxLatLon(),
+                "-p", String.valueOf(request.getPower()),
+                "-m", request.getMode()
+        );
+
+        LOG.info("Executing command: {}", String.join(" ", command));
+
+        try {
+            ProcessBuilder pb = new ProcessBuilder(command);
+            pb.redirectErrorStream(true); 
+            Process process = pb.start();
+
+            // TODO: decided what to do with output
+            StringBuilder output = new StringBuilder();
+            try (var reader = new java.io.BufferedReader(new java.io.InputStreamReader(process.getInputStream()))) {
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    output.append(line).append("\n");
+                }
+            }
+
+            int exitCode = process.waitFor();
+            LOG.info("et-voacap process exited with code: {}", exitCode);
+
+            if (exitCode != 0) {
+                LOG.error("et-voacap failed with exit status: {}", exitCode);
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(List.of());
+            }
+
+        } catch (Exception e) {
+            LOG.error("Failed to run et-vocacap", e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(List.of());
+        }
+
+	// Assume that the run was successful and read the output
 	// TODO push into properties and use HOME env var
         final String path = "/home/gaston/itshfbc/run/voacapx.out";
 
         try {
             List<PredictionHour> hours = parser.parse(path);
-
             LOG.info("Parsed {} prediction hours", hours.size());
 
             return ResponseEntity.ok(hours);
